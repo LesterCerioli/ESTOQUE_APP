a088e0c5048635c4664bceed82dd7f60bcf5a13b
Developers LucasTecnologia <lesterlucasit@hotmail.com>
2018-09-04 19:43:31 -0300

    initial imported

---
+++ DAL/DadosDaConexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexao
+    {
+        public static String _StringDeConexao
+        {
+            get
+            {
+                return "Data Source=DESKTOP-NNGF7B2\\SQLEXPRESS;Initial Catalog=estoque.db;User ID=lucas;Password=Fuy74367908";
+            }
+        }
+    }
+}
